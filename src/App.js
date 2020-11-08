f3cc90331b4ef2af48e49ca98121bfc0bb0502d9
Ram Akunuru <ram@ingenious.ai>
2020-11-08 15:33:08 +1100

    added back squash

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn git
+          Learn git squash
         </a>
       </header>
     </div>
