93fcf1e6601157371e0156d89e55979c7087cba7
Ram Akunuru <ram@ingenious.ai>
2020-11-08 15:26:35 +1100

    learning git squash

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn git squash
         </a>
       </header>
     </div>
